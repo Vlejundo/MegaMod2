259b7d1736357e7c9186f4ffc319ecc60e3ff9af
SK <ge.heim822@gmail.com>
2017-11-03 20:28:10 +0100

    Unnötige Module gelöscht

--- Source - Floris Basic Mod Pack/Module/mergefiles.py
+++
@@ -1,1 +0,0 @@
-file(".\module_items.py",'wb').write(file("..\Source\Source - Floris Basic Mod Pack\Module\module_items.py",'rb').read()+file(".\mergeitems.py",'rb').read())
--- Source - Floris Basic Mod Pack/Module/mergeitems.py
+++
@@ -1,9 +0,0 @@
-# modmerger_start version=201 type=2
-try:
-    component_name = "items"
-    var_set = { "items" : items }
-    from modmerger import modmerge
-    modmerge(var_set)
-except:
-    raise
-# modmerger_end
